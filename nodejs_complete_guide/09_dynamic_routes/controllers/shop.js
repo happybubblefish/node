de2b7ca5fbf0f971550e7c4b112b2f799cf94207
happybubblefish <congls2007@yahoo.com>
2019-01-03 08:20:02 -0500

    add part of module 09

--- nodejs_complete_guide/09_dynamic_routes/controllers/shop.js
+++ nodejs_complete_guide/09_dynamic_routes/controllers/shop.js
@@ -10,6 +10,17 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const productId = req.params.productId;
+  Product.findById(productId, product => {
+    res.render('shop/product-detail', {
+      product,
+      pageTitle: product.title,
+      path: '/product'
+    });
+  });
+}
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
@@ -26,6 +37,12 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
+
+exports.postCart = (req, res, next) => {
+  const productId = req.body.productId; //from hidden field
+  console.log(productId);
+  res.redirect('/cart');
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
